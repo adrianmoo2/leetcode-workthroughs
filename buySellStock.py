6ce5ecfbdde5438cbd1b82ba419c2741e3bfdf40
adrianmoo2 <atran059@ucr.edu>
2019-06-27 09:24:46 -0700

    Functional buySellStock

---
+++ buySellStock.py
@@ -0,0 +1,10 @@
+def maxProfit(prices):
+    if not prices: return 0
+    lp, p = prices[0], 0
+    
+    for i in range(1, len(prices)):
+        p = max(p, prices[i]-lp)
+        lp = min(lp, prices[i])
+            
+    return p
+        
