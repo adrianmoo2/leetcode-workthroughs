cc8bf7e79d08cc9d9fb70eb5a702c3df466eba10
adrianmoo2 <atran059@ucr.edu>
2019-07-04 14:36:07 -0700

    added iterative solution

--- invertBinaryTree.py
+++ invertBinaryTree.py
@@ -15,4 +15,24 @@ def invertTree(self, root):
         self.invertTree(curr.right)
         
     return root
+
+def invertTreeIterative(self, root):
+    """
+    :type root: TreeNode
+    :rtype: TreeNode
+    """
+
+    node_list = [root]
+    
+    while node_list:
+        node_pop = node_list.pop()
+        
+        if node_pop:
+            temp_node = node_pop.left
+            node_pop.left = node_pop.right
+            node_pop.right = temp_node
+
+            node_list.extend((node_pop.left, node_pop.right))
+    
+    return root
             
