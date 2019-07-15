c29010e9b1686996423db3f613897d2a28d5c6e9
adrianmoo2 <atran059@ucr.edu>
2019-07-14 23:15:14 -0700

    functional firstUniqueCharacterInString

---
+++ firstUniqueCharacterInString.py
@@ -0,0 +1,21 @@
+def firstUniqueChar(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    seen_map = {}
+
+    for i,v in enumerate(list(s)):
+        if v in seen_map:
+            seen_map[v] += 1
+        else:
+            seen_map[v] = 1
+    
+    print (seen_map)
+
+    for i,v in enumerate(list(s)):
+        if seen_map[v] == 1:
+            return i
+    return -1
+
+firstUniqueChar("loveleetcode")
