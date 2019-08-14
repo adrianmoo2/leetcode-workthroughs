abb820fa0ca5591acdcf7e24fb2aeef22e523e92
adrianmoo2 <atran059@ucr.edu>
2019-08-13 22:51:18 -0700

    functional 3sum

--- 3sum.py
+++ 3sum.py
@@ -12,18 +12,56 @@ def threeSum(nums):
         
         for j in range(len(nums)):
             if j == i:
-                break
+                continue
             else:
                 if nums[j] in seen:
                     seen[nums[j]] += 1
                 else:
                     seen[nums[j]] = 1
-                    
-                if -(nums[i] +(nums[j] * seen[nums[j]])) == nums[j]:
-                    break
-                elif -(nums[i] + (nums[j] * seen[nums[j]])) in seen:
-                        res_list.append([nums[i], nums[j], -(nums[i] + (nums[j] * seen[nums[j]]))])
+                
+                third_num = -(nums[i] +(nums[j] * seen[nums[j]]))
+                print ("nums[i]: " + str(nums[i]))
+                print ("nums[j]: " + str(nums[j]) + ": " + str(seen[nums[j]]))
+                print ("third_num: " + str(third_num) + "\n")
+
+                if third_num in seen and seen[third_num] >= 2:
+                        print ("added\n")
+                        res_list.append([nums[i], nums[j], third_num])
+                        seen[third_num] -= 1
                 
     return res_list
+
+def threeSumOptimal(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    res = []
+    nums.sort()
+    
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        else:
+            l, r = i+1, len(nums)-1
+            
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                
+                if s > 0:
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    
+                    l += 1
+                    r -= 1
+    return res
 
 print (str(threeSum([-1, 0, 1, 2, -1, -4])))
