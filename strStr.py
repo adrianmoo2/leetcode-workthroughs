97e83ffe06c3ee044a1af5ea3edd8fa9bb5c5f19
adrianmoo2 <atran059@ucr.edu>
2019-08-14 22:58:37 -0700

    attempted strstr

---
+++ strStr.py
@@ -0,0 +1,10 @@
+def strStr(haystack, needle):
+    """
+    :type haystack: str
+    :type needle: str
+    :rtype: int
+    """
+    l, r = 0, len(haystack)-1
+                
+    
+strStr("hello", "ll")
